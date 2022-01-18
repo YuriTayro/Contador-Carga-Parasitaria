2cb688b109e6f7d014e7bec69a3b97d37bb9667c
Yuri Tayro <96271669+YuriTayro@users.noreply.github.com>
2022-01-17 23:32:58 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,26 @@
+let contadorAmas = 0
+let contadorMacro = 0   
+let res1 = window.document.querySelector('div#resMacro') 
+let res2 = window.document.querySelector('div#resAmas')  
+
+function clicar1(){
+    
+    contadorMacro ++ 
+    let but1 = window.document.querySelector('input#butMacro')
+    res1.innerHTML = `<p>O contador de Macrófago está com <mark>${contadorMacro}</mark>cliques</p>`
+}
+
+function clicar2(){
+    
+    contadorAmas ++
+    let but2 = window.document.querySelector('input#butAmas')    
+    res2.innerHTML = `<p>O contador de Amastigota está com <mark>${contadorAmas}</mark>cliques</p>`
+}
+
+function clicar3(){
+    contadorAmas = 0
+    contadorMacro = 0
+    let but3 = window.document.querySelector('input#butZer')
+    res1.innerHTML = null
+    res2.innerHTML = null 
+}
